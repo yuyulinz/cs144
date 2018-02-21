250fac3272d4017efe42f612ee84bf860011e513
Yulin Zhang <compadmin@admins-MacBook-Pro.local>
2018-02-20 20:00:48 -0800

    added commonmark

--- project3/angular-blog/src/app/preview/preview.component.ts
+++ project3/angular-blog/src/app/preview/preview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post, BlogService } from '../blog.service';
 import { Parser, HtmlRenderer } from 'commonmark';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as commonmark from "commonmark";
 
 @Component({
   selector: 'app-preview',
@@ -22,14 +23,23 @@ export class PreviewComponent implements OnInit {
 
   ngOnInit() {
     this.getPost();
+    var reader = new commonmark.Parser();
   }
 
   getPost(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.post = this.blogService.getPost(id);
-    this.title = this.post.title;
-    this.body = this.post.body;
+
+    var reader = new commonmark.Parser();
+    var writer = new commonmark.HtmlRenderer();
+    var title_parsed = reader.parse(this.post.title);
+    var title_result = writer.render(title_parsed);
+
+    var body_parsed = reader.parse(this.post.body);
+    var body_result = writer.render(body_parsed);
+
+    this.title = title_result;
+    this.body = body_result;
+    console.log(body_result);
   }
-
-
 }
